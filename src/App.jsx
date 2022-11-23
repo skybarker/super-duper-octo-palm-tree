b846107a1bbf2dceb848c7190e00b1314f97e11d
skybarker <112577008+skybarker@users.noreply.github.com>
2022-11-22 21:27:42 -0600

    Refactor for useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import useFetch from "./hooks/useFetch";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const users = useFetch();
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
